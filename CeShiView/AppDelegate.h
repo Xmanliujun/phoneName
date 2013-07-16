6e0918943358f104a33b59f9d01ee74b5252fe43
Xmanliujun <644240319@qq.com>
2013-07-16 16:35:18 +0800

    Initial Commit

---
+++ CeShiView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CeShiView
+//
+//  Created by 刘军 on 13-7-16.
+//  Copyright (c) 2013年 刘军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CeShiView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CeShiView
+//
+//  Created by 刘军 on 13-7-16.
+//  Copyright (c) 2013年 刘军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
